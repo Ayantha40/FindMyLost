5e66408e9565cc4b365e6325c12f0d73cf0e2897
ILANGESHWARAN SULAKSHI <cb010434@students.apiit.lk>
2023-01-24 14:13:39 +0530

    FindMyLost

---
+++ FindMyLost/FindMyLost/RegisterEmployees.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FindMyLost
+{
+    public partial class RegisterEmployees : Form
+    {
+        public RegisterEmployees()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEditUserPic_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
