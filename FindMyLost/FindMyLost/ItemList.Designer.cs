277715730000cfed7be31e350d30e5181bdc0f52
SULAKSHI <cb010434@students.apiit.lk>
2023-01-28 18:55:32 +0530

    Item List

---
+++ FindMyLost/FindMyLost/ItemList.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace FindMyLost
+{
+    partial class ItemList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvItemList = new MaterialSkin.Controls.MaterialListView();
+            this.ItemID = new System.Windows.Forms.ColumnHeader();
+            this.Category = new System.Windows.Forms.ColumnHeader();
+            this.Color = new System.Windows.Forms.ColumnHeader();
+            this.Brand = new System.Windows.Forms.ColumnHeader();
+            this.LastSeen = new System.Windows.Forms.ColumnHeader();
+            this.SuspendLayout();
+            // 
+            // lvItemList
+            // 
+            this.lvItemList.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lvItemList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ItemID,
+            this.Category,
+            this.Color,
+            this.Brand,
+            this.LastSeen});
+            this.lvItemList.Depth = 0;
+            this.lvItemList.Font = new System.Drawing.Font("Montserrat", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lvItemList.FullRowSelect = true;
+            this.lvItemList.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.lvItemList.HideSelection = false;
+            this.lvItemList.Location = new System.Drawing.Point(16, 70);
+            this.lvItemList.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.lvItemList.MouseLocation = new System.Drawing.Point(-1, -1);
+            this.lvItemList.MouseState = MaterialSkin.MouseState.OUT;
+            this.lvItemList.Name = "lvItemList";
+            this.lvItemList.OwnerDraw = true;
+            this.lvItemList.Size = new System.Drawing.Size(850, 600);
+            this.lvItemList.TabIndex = 1;
+            this.lvItemList.UseCompatibleStateImageBehavior = false;
+            this.lvItemList.View = System.Windows.Forms.View.Details;
+            this.lvItemList.MouseClick += new System.Windows.Forms.MouseEventHandler(this.lvItemList_MouseClick);
+            // 
+            // ItemID
+            // 
+            this.ItemID.Text = "Item ID";
+            this.ItemID.Width = 110;
+            // 
+            // Category
+            // 
+            this.Category.Text = "Category";
+            this.Category.Width = 170;
+            // 
+            // Color
+            // 
+            this.Color.Text = "Color";
+            this.Color.Width = 120;
+            // 
+            // Brand
+            // 
+            this.Brand.Text = "Brand";
+            this.Brand.Width = 180;
+            // 
+            // LastSeen
+            // 
+            this.LastSeen.Text = "Last Seen Location";
+            this.LastSeen.Width = 270;
+            // 
+            // ItemList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(879, 688);
+            this.Controls.Add(this.lvItemList);
+            this.Font = new System.Drawing.Font("Montserrat", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "ItemList";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ITEM LIST";
+            this.Load += new System.EventHandler(this.ItemList_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialListView lvItemList;
+        private System.Windows.Forms.ColumnHeader ItemID;
+        private System.Windows.Forms.ColumnHeader Category;
+        private System.Windows.Forms.ColumnHeader Color;
+        private System.Windows.Forms.ColumnHeader Brand;
+        private System.Windows.Forms.ColumnHeader LastSeen;
+    }
+}
---
+++ FindMyLost/FindMyLost/ItemList.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaterialSkin;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace FindMyLost
+{
+    public partial class ItemList : MaterialSkin.Controls.MaterialForm
+    {
+        MaterialSkinManager skinManager;
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConString"].ToString());
+
+        public ItemList()
+        {
+            InitializeComponent();
+            skinManager = MaterialSkinManager.Instance;
+            skinManager.AddFormToManage(this);
+            skinManager.Theme = MaterialSkinManager.Themes.DARK;
+            skinManager.ColorScheme = new ColorScheme(Primary.Green800, Primary.Green700, Primary.Green700, Accent.LightBlue100, TextShade.WHITE);
+        }
+
+        private void ItemList_Load(object sender, EventArgs e)
+        {
+            lvItemList.FullRowSelect = true;
+
+            try
+            {
+                string sql = "SELECT item_id, item_category, item_colour, item_brand, last_seen_location FROM Lost_Item";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                conn.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    ListViewItem item = new ListViewItem(dr.GetInt32(0).ToString());
+                    item.SubItems.Add(dr.GetString(1));
+                    item.SubItems.Add(dr.GetString(2));
+                    item.SubItems.Add(dr.GetString(3));
+                    item.SubItems.Add(dr.GetString(4));
+
+                    lvItemList.Items.Add(item);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "FindMyLost", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public static string SelectedItemID;
+
+        private void lvItemList_MouseClick(object sender, MouseEventArgs e)
+        {
+            ListViewItem item = lvItemList.SelectedItems[0];
+            SelectedItemID = item.SubItems[0].Text;
+            ItemProfile ip = new ItemProfile();
+            ip.Show();
+        }
+
+        
+    }
+}
