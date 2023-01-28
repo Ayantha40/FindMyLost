3ca8f14c155bd7ec43f75afe51b413ff7b51472e
SULAKSHI <cb010434@students.apiit.lk>
2023-01-28 22:23:49 +0530

    Claim List

---
+++ FindMyLost/FindMyLost/ClaimList.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace FindMyLost
+{
+    partial class ClaimList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvClaimList = new MaterialSkin.Controls.MaterialListView();
+            this.claimID = new System.Windows.Forms.ColumnHeader();
+            this.ClaimerName = new System.Windows.Forms.ColumnHeader();
+            this.ItemCategory = new System.Windows.Forms.ColumnHeader();
+            this.ItemColour = new System.Windows.Forms.ColumnHeader();
+            this.ItemBrand = new System.Windows.Forms.ColumnHeader();
+            this.LastSeen = new System.Windows.Forms.ColumnHeader();
+            this.SuspendLayout();
+            // 
+            // lvClaimList
+            // 
+            this.lvClaimList.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lvClaimList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.claimID,
+            this.ClaimerName,
+            this.ItemCategory,
+            this.ItemColour,
+            this.ItemBrand,
+            this.LastSeen});
+            this.lvClaimList.Depth = 0;
+            this.lvClaimList.Font = new System.Drawing.Font("Montserrat", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lvClaimList.FullRowSelect = true;
+            this.lvClaimList.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.lvClaimList.HideSelection = false;
+            this.lvClaimList.Location = new System.Drawing.Point(16, 70);
+            this.lvClaimList.MouseLocation = new System.Drawing.Point(-1, -1);
+            this.lvClaimList.MouseState = MaterialSkin.MouseState.OUT;
+            this.lvClaimList.Name = "lvClaimList";
+            this.lvClaimList.OwnerDraw = true;
+            this.lvClaimList.Size = new System.Drawing.Size(1000, 699);
+            this.lvClaimList.TabIndex = 0;
+            this.lvClaimList.UseCompatibleStateImageBehavior = false;
+            this.lvClaimList.View = System.Windows.Forms.View.Details;
+            // 
+            // claimID
+            // 
+            this.claimID.Text = "Claim ID";
+            this.claimID.Width = 120;
+            // 
+            // ClaimerName
+            // 
+            this.ClaimerName.Text = "Name";
+            this.ClaimerName.Width = 200;
+            // 
+            // ItemCategory
+            // 
+            this.ItemCategory.Text = "Category";
+            this.ItemCategory.Width = 150;
+            // 
+            // ItemColour
+            // 
+            this.ItemColour.Text = "Colour";
+            this.ItemColour.Width = 120;
+            // 
+            // ItemBrand
+            // 
+            this.ItemBrand.Text = "Brand";
+            this.ItemBrand.Width = 180;
+            // 
+            // LastSeen
+            // 
+            this.LastSeen.Text = "Last Seen Location";
+            this.LastSeen.Width = 230;
+            // 
+            // ClaimList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1032, 781);
+            this.Controls.Add(this.lvClaimList);
+            this.Name = "ClaimList";
+            this.Text = "CLAIM LIST";
+            this.Load += new System.EventHandler(this.ClaimList_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialListView lvClaimList;
+        private System.Windows.Forms.ColumnHeader claimID;
+        private System.Windows.Forms.ColumnHeader ClaimerName;
+        private System.Windows.Forms.ColumnHeader ItemCategory;
+        private System.Windows.Forms.ColumnHeader ItemColour;
+        private System.Windows.Forms.ColumnHeader ItemBrand;
+        private System.Windows.Forms.ColumnHeader LastSeen;
+    }
+}
---
+++ FindMyLost/FindMyLost/ClaimList.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaterialSkin;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace FindMyLost
+{
+    public partial class ClaimList : MaterialSkin.Controls.MaterialForm
+    {
+        MaterialSkinManager skinManager;
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConString"].ToString());
+
+        public ClaimList()
+        {
+            InitializeComponent();
+            skinManager = MaterialSkinManager.Instance;
+            skinManager.AddFormToManage(this);
+            skinManager.Theme = MaterialSkinManager.Themes.DARK;
+            skinManager.ColorScheme = new ColorScheme(Primary.Green800, Primary.Green700, Primary.Green700, Accent.LightBlue100, TextShade.WHITE);
+        }
+
+        private void ClaimList_Load(object sender, EventArgs e)
+        {
+            lvClaimList.FullRowSelect = true;
+
+            try
+            {
+                string sql = "SELECT claim_id, claimer_name, item_category, item_colour, item_brand, last_Seen_location FROM Claim";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                conn.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    ListViewItem item = new ListViewItem(dr.GetInt32(0).ToString());
+                    item.SubItems.Add(dr.GetString(1));
+                    item.SubItems.Add(dr.GetString(2));
+                    item.SubItems.Add(dr.GetString(3));
+                    item.SubItems.Add(dr.GetString(4));
+                    item.SubItems.Add(dr.GetString(5));
+
+                    lvClaimList.Items.Add(item);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "FindMyLost", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
+}
