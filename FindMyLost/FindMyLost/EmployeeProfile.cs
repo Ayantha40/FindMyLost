510b0279bcfc2edda6b34327cb8333a007644eef
Ayantha <122976994+Ayantha@users.noreply.github.com>
2023-01-29 07:44:22 +0530

    Employee Profile GUI

--- FindMyLost/FindMyLost/EmployeeProfile.cs
+++ FindMyLost/FindMyLost/EmployeeProfile.cs
@@ -54,5 +54,25 @@ namespace FindMyLost
 
             }
         }
+
+        private void lblAddress_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EmployeeProfile_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
     }
 }
